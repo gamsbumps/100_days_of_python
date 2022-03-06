6daf4b0cb8dd824dde1f17808fe7b57d64a25b10
gamsbumps <guilhermecc2@hotmail.com>
2022-03-06 20:45:34 -0300

    Higher_lower_start corrections

--- higher_lower_start.py
+++ higher_lower_start.py
@@ -3,6 +3,7 @@ from game_data import data
 import os
 import random
 
+
 clear = lambda: os.system('cls')
 
 score = 0
@@ -10,12 +11,15 @@ account_a = random.choice(data)
 account_b = random.choice(data)
 if account_a == account_b:
     account_a = random.choice(data)
+
 guessing_right = True
 while guessing_right:
     clear()
     print(logo_d)
     if score > 0:
         print(f"You are right! Current score: {score}.")
+    if account_a == account_b:
+        account_a = random.choice(data)
     print(f"Compare A: {account_a['name']}, a {account_a['description']} from {account_a['country']}")
     print(vs)
     print(f"Against B: {account_b['name']}, a {account_b['description']} from {account_b['country']}")
